e4feba1c0120f13b4e96a7294fe8ebea125c3ebb
Themba-Dombo <themba.dombo@gmail.com>
2020-05-18 13:20:07 +0200

    edited password is ok tests and used prettier formatter on all the files

--- spec/password_is_valid_spec.js
+++ spec/password_is_valid_spec.js
@@ -1,27 +1,39 @@
-const {passwordIsValid} = require("../src/password_checker.js");
+const { passwordIsValid } = require("../src/password_checker.js");
 
-describe("passwordIsValid", ()=>{
-    it("shouldn't allow a empty string", ()=>{
-        expect(()=>passwordIsValid("")).toThrow(new Error("password should exist"));
-    });
+describe("passwordIsValid", () => {
+  it("shouldn't allow a empty value", () => {
+    expect(() => passwordIsValid()).toThrow(new Error("password should exist"));
+  });
 
-    it("shouldn't accept a string less than 9 characters", ()=>{
-        expect(()=>{passwordIsValid("#th3mb@D")}).toThrow(new Error("password should be longer than than 8 characters"));
-    });
+  it("shouldn't accept a string less than 9 characters", () => {
+    expect(() => {
+      passwordIsValid("#th3mb@D");
+    }).toThrow(new Error("password should be longer than than 8 characters"));
+  });
 
-    it("shouldn't allow uppercase characters only", ()=>{
-        expect(()=>{passwordIsValid("THEMB@123D")}).toThrow(new Error("password should have at least one lowercase letter"));
-    });
+  it("shouldn't allow uppercase characters only", () => {
+    expect(() => {
+      passwordIsValid("THEMB@123D");
+    }).toThrow(new Error("password should have at least one lowercase letter"));
+  });
 
-    it("shouldn't allow lowercase characters only", ()=>{
-        expect(()=>{passwordIsValid("themb@dombo2")}).toThrow(new Error("password should have at least one uppercase letter"));
-    });
+  it("shouldn't allow lowercase characters only", () => {
+    expect(() => {
+      passwordIsValid("themb@dombo2");
+    }).toThrow(new Error("password should have at least one uppercase letter"));
+  });
 
-    it("shouldn't only contain alphabetic characters", ()=>{
-        expect(()=>{passwordIsValid("ThembaDombo")}).toThrow(new Error("password should at least have one digit"));
-    });
+  it("shouldn't only contain alphabetic characters", () => {
+    expect(() => {
+      passwordIsValid("ThembaDombo");
+    }).toThrow(new Error("password should at least have one digit"));
+  });
 
-    it("shouldn't only contain alphanumeric characters", ()=>{
-        expect(()=>{passwordIsValid("Themb8Dombo")}).toThrow(new Error("password should have at least one special character"));
-    });
+  it("shouldn't only contain alphanumeric characters", () => {
+    expect(() => {
+      passwordIsValid("Themb8Dombo");
+    }).toThrow(
+      new Error("password should have at least one special character")
+    );
+  });
 });
--- src/password_checker.js
+++ src/password_checker.js
@@ -1,11 +1,27 @@
-function passwordIsValid(password){
-    throw new Error("password should exist");
+function passwordIsValid(password) {
+  if (password == null) throw new Error("password should exist");
+
+  if (/.{9}/.test(password) == false)
+    throw new Error("password should be longer than than 8 characters");
+
+  if (/[a-z]/.test(password) == false)
+    throw new Error("password should have at least one lowercase letter");
+
+  if (/[A-Z]/.test(password) == false)
+    throw new Error("password should have at least one uppercase letter");
+
+  if (/[0-9]/.test(password) == false)
+    throw new Error("password should at least have one digit");
+
+  if (/[^0-9a-zA-Z]/.test(password) == false)
+    throw new Error("password should have at least one special character");
 }
 
-function passwordIsOk(password){
+function passwordIsOk(password) {
+  if ((password != null) & /.{9}/.test(password) & /[a-z]/.test(password))
+    return true;
 
+  return false;
 }
 
-// console.log(passwordIsValid("hello97"))
-
-module.exports = {passwordIsValid, passwordIsOk};
+module.exports = { passwordIsValid, passwordIsOk };
